0ab8637e7d769277413fa430b21dcadd463662f9
Loach1703 <ctz2012@outlook.com>
2021-07-24 14:51:35 +0800

    新的登录注册页面

--- civitas web/statics/js/login/onload.js
+++ civitas web/statics/js/login/onload.js
@@ -3,6 +3,11 @@ function load_updata()
 {
     //获取日期，天气
     load_date();
+    //如果登录了，跳转到主页
+    if (is_login() == 1)
+    {
+        window.location.href = "index.html";
+    }
 }
 
 window.onload = load_updata;
