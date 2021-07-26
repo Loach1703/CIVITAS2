6eea74ffac62a549d3e5b997f0fd56e12f5bcb2a
feng wen <2324465096@qq.com>
2021-07-26 16:55:28 +0800

    更新了登出和找回链接

--- civitas web/civitas/urls.py
+++ civitas web/civitas/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,re_path
 from . import views
-from . import rspeech
+
 from . import map
 from . import test
 from django.conf.urls import include
@@ -29,33 +29,24 @@ urlpatterns = [
     path('test/',test.test),
     path('login.html',views.login),
     path('blog/1.html',views.blog1),
-    path('register.html',views.register)
-]
-# urlpatterns += [
-#     path('accounts/', include('django.contrib.auth.urls')),
+    path('register.html',views.register),
     
-# ]
-#使用内置轮子进行登入和找回
-#如果独立轮子，参考https://www.cnblogs.com/derek1184405959/p/8567522.html
-urlpatterns += [
-    path('', include('django.contrib.auth.urls')),
-    # path('login/',loginViews.LoginView.as_view(), name='login'),
-    # path('logout/',loginViews.LogoutView.as_view(), name='logout'),
-
-    # path('password-reset/', loginViews.PasswordResetView.as_view(), name='password_reset'),
-    # path('password-reset/done/', loginViews.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', loginViews.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', loginViews.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('login',loginViews.LoginView.as_view(), name='login'),
 ]
-# 增加验证码的路径
-# urlpatterns +=[
-#     path('captcha', include('captcha.urls'))
-# ]
+
+urlpatterns += [
+    
+
+    path('password-reset/', loginViews.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', loginViews.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', loginViews.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', loginViews.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-change/', loginViews.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', loginViews.PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
+
 urlpatterns +=[
     
-    # path('regis/', views.regisview, name='regis'),
-    path('logout/',loginViews.LogoutView.as_view(), name='logout'),
+    path('logout.html',views.logout)
 
 
 ]
--- civitas web/civitas/views.py
+++ civitas web/civitas/views.py
@@ -18,50 +18,6 @@ def views(request):
     list2=list(dict1.values())
     return render(request, "index.html", {"list":list2})
 
-def regisview(request):
-  return render(request,"regis/regis.html")
-
-def register(request):
-    # if request.session.get('is_login', None):
-    #     # 登录状态不允许注册。你可以修改这条原则！
-    #     return redirect("/index/")
-    # if request.method == "POST":
-    #     register_form = RegisterForm(request.POST)
-    #     message = "请检查填写的内容！"
-    #     if register_form.is_valid():  # 获取数据
-    #         username = register_form.cleaned_data['username']
-    #         password1 = register_form.cleaned_data['password1']
-    #         password2 = register_form.cleaned_data['password2']
-    #         email = register_form.cleaned_data['email']
-    #         sex = register_form.cleaned_data['sex']
-    #         if password1 != password2:  # 判断两次密码是否相同
-    #             message = "两次输入的密码不同！"
-    #             return render(request, 'user19/register.html', locals())
-    #         else:
-    #             same_name_user = models.User.objects.filter(name=username)
-    #             if same_name_user:  # 用户名唯一
-    #                 message = '用户已经存在，请重新选择用户名！'
-    #                 return render(request, 'user19/register.html', locals())
-    #             same_email_user = models.User.objects.filter(email=email)
-    #             if same_email_user:  # 邮箱地址唯一
-    #                 message = '该邮箱地址已被注册，请使用别的邮箱！'
-    #                 return render(request, 'user19/register.html', locals())
-  
-    #             # 当一切都OK的情况下，创建新用户
-  
-    #             new_user = models.User.objects.create()
-    #             new_user.name = username
-    #             new_user.password = password1
-    #             new_user.email = email
-    #             new_user.sex = sex
-    #             new_user.save()
-    #             return redirect('/login/')  # 自动跳转到登录页面
-    # register_form = RegisterForm()
-    form = NameForm(request)
-    your_name = forms.CharField(label='Your name', max_length=100)
-
-    #return render(request,"regis/regis.html", locals())
-    return render(request, 'regis/regis.html', {'form': form})
 def login(req):
     return render(req, "login/login.html")
 
@@ -70,3 +26,6 @@ def blog1(req):
 
 def register(req):
     return render(req, "login/register.html")
+
+def logout(req):
+    return render(req, "registration/logout.html")
