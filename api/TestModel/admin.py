def585bc74fe8621e8c405fff184ff545a95945e
feng wen <2324465096@qq.com>
2021-07-27 21:44:00 +0800

    添加了四维模型和后台注册

--- api/TestModel/admin.py
+++ api/TestModel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.speech)
+admin.site.register(models.weather)
+admin.site.register(models.usersession)
+admin.site.register(models.speech_attitude)
+admin.site.register(models.personal_attributes)
