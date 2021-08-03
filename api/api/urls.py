d2ca941629e51d7b0de9b71c06a458d60589e45c
feng wen <2324465096@qq.com>
2021-08-03 19:04:00 +0800

    url.py add user

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . import user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
