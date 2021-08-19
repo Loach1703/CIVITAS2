f84885f4cab76ec1f3f53f5145a405bb481a534d
fengwewn <2324465096@qq.com>
2021-08-19 12:08:27 +0800

    admin更新

--- api/models/UserModel/admin.py
+++ api/models/UserModel/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import usersession,Avatar,personal_attributes
 # Register your models here.
-
+class attributes(admin.ModelAdmin):
+    list_display = ('uid','energy','healthy')
 admin.site.register(usersession)
 admin.site.register(Avatar)
 admin.site.register(personal_attributes)
