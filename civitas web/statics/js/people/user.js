c411dd7a8c5dc6527166d9fb15e7de3e6eea9d6f
Loach1703 <ctz2012@outlook.com>
2021-09-13 22:21:09 +0800

    flex

--- civitas web/statics/js/people/user.js
+++ civitas web/statics/js/people/user.js
@@ -3,7 +3,7 @@
 
 组件1
 名称:people-detail
-用途:显示演讲，翻页
+用途:个人主页中显示个人详细信息
 props:{
     prop:{
         uid:对应用户的uid
@@ -16,19 +16,18 @@ Vue.component("people-detail", {
     props: ["prop"],
     data: function () {
         return {
-            username: ""
         }
     },
     created: function () {
-        this.username = this.prop.username;
-        document.title = this.username + "的主页 - 古典社会模拟 CIVITAS2";
+        document.title = this.prop.username + "的主页 - 古典社会模拟 CIVITAS2";
     },
     watch: {
         prop: {
-            handler: function () {
+            handler: function (val,oldVal) {
                 console.log(this.prop)
-                this.username = this.prop.username;
-                document.title = this.username + "的主页 - 古典社会模拟 CIVITAS2";
+                console.log(val)
+                console.log(oldVal)
+                document.title = this.prop.username + "的主页 - 古典社会模拟 CIVITAS2";
             },
             deep: true
         }
@@ -38,9 +37,9 @@ Vue.component("people-detail", {
         <img v-bind:src="'https://api.trickydeath.xyz/getavatar/?uid='+prop.uid" class="img-thumbnail" width="100px" height="100px">
         <div class="main-people-text">
             <p>{{ prop.test }}</p>
-            <p>{{ username }}</p>
-            <p class="author">>位于京兆尹，长安县 >籍贯京兆尹</p>
-            <p class="author">><a v-bind:href="'depository.html?uid='+prop.uid">{{ username }}的库房</a></p>
+            <p>{{ prop.username }}</p>
+            <p class="explain">>位于<a v-bind:href="'city.html?uid='+prop.cityid">京兆尹</a>，长安县 >籍贯<a v-bind:href="'city.html?uid='+prop.cityid">京兆尹</a></p>
+            <p class="explain">><a v-bind:href="'depository.html?uid='+prop.uid">{{ prop.username }}的库房</a></p>
         </div>
     </div>
     `
--- civitas web/statics/js/recipe/recipe.js
+++ civitas web/statics/js/recipe/recipe.js
@@ -1,169 +1,184 @@
-var recipe_vm;
+/*
+个人主页Vue组件
 
-function load_recipe()
-{
-    Vue.component('ingredient-one', {
-        props: ["ingredient_list","used_ingredients","ingredient_prop","index"],
-        data: function () {
-            return {
-                ingredient_number: "",
-                ingredient_id: -1,
+组件1
+名称:people-detail
+用途:个人主页中显示个人详细信息
+props:{
+    prop:{
+        uid:对应用户的uid
+        username:对应用户的用户名
+    },
+}
+*/
+
+Vue.component('ingredient-detail', {
+    props: ["ingredient_list","used_ingredients","ingredient_prop","index"],
+    data: function () {
+        return {
+            ingredient_number: "",
+            ingredient_id: -1,
+            ingredient_quality: 1
+        }
+    },
+    methods: {
+        ingredient_change: function () {
+            if (this.ingredient_id == -1) 
+            {
+                return
             }
+            this.$emit("ingredient_change",this.index,this.ingredient_id,this.ingredient_number);
         },
-        methods: {
-            ingredient_change: function () {
-                if (this.ingredient_id == -1) 
-                {
-                    return
-                }
-                this.$emit("ingredient_change",this.index,this.ingredient_id,this.ingredient_number);
-            },
-            remove_ingredients: function () {
-                this.$emit("remove", this.index);
+        remove_ingredients: function () {
+            this.$emit("remove", this.index);
+        }
+    },
+    watch: {
+        ingredient_id: function () {
+            this.ingredient_change();
+        },
+        ingredient_number: function (value,old_value) {
+            if (isNaN(value))
+            {
+                this.ingredient_number = old_value;
             }
-        },
-        watch: {
-            ingredient_id: function () {
-                this.ingredient_change();
-            },
-            ingredient_number: function (value,old_value) {
-                if (isNaN(value))
-                {
-                    this.ingredient_number = old_value;
-                }
-                if (value < 0)
-                {
-                    this.ingredient_number = 0;
-                }
-                else if (value > this.ingredient_prop[this.ingredient_id].max)
-                {
-                    this.ingredient_number = this.ingredient_prop[this.ingredient_id].max;
-                }
-                this.ingredient_number = this.ingredient_number = Math.round(this.ingredient_number * 100) / 100;
-                this.ingredient_change();
+            if (value < 0)
+            {
+                this.ingredient_number = 0;
             }
-        },
-        template: `
-        <div class="recipe-single bottomline-dashed">
-            <img v-bind:src="'civitas/icon/goods/'+ingredient_id+'.png'" class="float-left" width="60px" height="60px"/>
-            <div class="recipe-name col-lg-2">
-                <select class="form-control" v-model="ingredient_id">
-                    <option disabled="disabled" value="-1">请选择</option>
-                    <option 
-                        v-for="(ingredient_mes,index) in ingredient_list" 
-                        v-bind:disabled="used_ingredients.includes(ingredient_mes.id)"
-                        v-bind:value="ingredient_mes.id" 
-                        v-bind:key="index">
-                        {{ ingredient_mes.name }}
-                    </option>
-                </select>
-            </div>
-            <div class="recipe-name col-lg-2">
-                <input type="number" 
-                    class="form-control" 
-                    v-model="ingredient_number" 
-                    step="0.01"
-                    placeholder="数量">
-                </input>
-            </div>
-            <div class="recipe-prop">
-                <p class="recipe-number">{{ (ingredient_prop[ingredient_id].starvation * ingredient_number).toFixed(2) }}<br></p>
-                <p class="recipe-starvation">饥饿变化</p>
-            </div>
-            <div class="recipe-prop">
-                <p class="recipe-number">{{ (ingredient_prop[ingredient_id].health * ingredient_number).toFixed(2) }}<br></p>
-                <p class="recipe-health">健康变化</p>
-            </div>
-            <div class="recipe-prop">
-                <p class="recipe-number">{{ (ingredient_prop[ingredient_id].stamina * ingredient_number).toFixed(2) }}<br></p>
-                <p class="recipe-stamina">精力变化</p>
-            </div>
-            <div class="recipe-taste">
-                <p class="recipe-taste-text">味道 
-                    <strong class="recipe-acid" v-if="ingredient_prop[ingredient_id].acid > 0">酸 </strong>
-                    <strong class="recipe-sweet" v-if="ingredient_prop[ingredient_id].sweet > 0">甜 </strong>
-                    <strong class="recipe-bitter" v-if="ingredient_prop[ingredient_id].bitter > 0">苦 </strong>
-                    <strong class="recipe-salt" v-if="ingredient_prop[ingredient_id].salt > 0">咸 </strong>
-                    <strong class="recipe-spice" v-if="ingredient_prop[ingredient_id].spice > 0">香料 </strong>
-                    <strong class="recipe-none" v-if="ingredient_prop[ingredient_id].acid == 0 &&
-                        ingredient_prop[ingredient_id].sweet == 0 &&
-                        ingredient_prop[ingredient_id].bitter == 0 &&
-                        ingredient_prop[ingredient_id].salt == 0 &&
-                        ingredient_prop[ingredient_id].spice == 0">无 
-                    </strong>
-                </p>
-            </div>
-            <button class="btn close float-right" v-on:click="remove_ingredients">&times;</button>
+            else if (value > this.ingredient_prop[this.ingredient_id].max)
+            {
+                this.ingredient_number = this.ingredient_prop[this.ingredient_id].max;
+            }
+            this.ingredient_number = this.ingredient_number = Math.round(this.ingredient_number * 100) / 100;
+            this.ingredient_change();
+        }
+    },
+    template: `
+    <div class="recipe-single bottomline-dashed">
+        <img v-bind:src="'civitas/icon/goods/'+ingredient_id+'.png'" class="float-left" width="60px" height="60px"/>
+        <div class="recipe-select">
+            <select class="form-control" v-model="ingredient_id">
+                <option disabled="disabled" value="-1">请选择</option>
+                <option 
+                    v-for="(ingredient_mes,index) in ingredient_list" 
+                    v-bind:disabled="used_ingredients.includes(ingredient_mes.id)"
+                    v-bind:value="ingredient_mes.id" 
+                    v-bind:key="index">
+                    {{ ingredient_mes.name }}
+                </option>
+            </select>
         </div>
-        `
-    })
-    Vue.component('ingredient-total', {
-        props: ["ingredient_total"],
-        data: function () {
-            return {
-                starvation: 0,
-                happiness: 0,
-                health: 0,
-                stamina: 0,
-                acid: 0,
-                sweet: 0,
-                bitter: 0,
-                salt: 0,
-                spice: 0
-            }
-        },
-        methods: {
-            get_total_props: function () {
-            }
-        },
-        watch: {
-            ingredient_total: function () {
-                console.log(this.ingredient_total);
-                this.get_total_props();
-            }
-        },
-        template: `
-        <div class="recipe-total bottomline-dashed">
-            <div class="recipe-total-name">
-                <p>属性合计</p>
-            </div>
+        <div class="recipe-select">
+            <input type="number" 
+                class="form-control" 
+                v-model="ingredient_number" 
+                step="0.01"
+                placeholder="数量">
+            </input>
+        </div>
+        <div class="recipe-select">
+            <select class="form-control" v-model="ingredient_quality">
+                <option value="1">Q1</option>
+                <option value="2">Q2</option>
+                <option value="3">Q3</option>
+            </select>
+        </div>
+        <div class="recipe-prop">
+            <p class="recipe-number">{{ (ingredient_prop[ingredient_id].starvation * ingredient_number).toFixed(2) }}<br></p>
+            <p class="recipe-prop-single">饥饿变化</p>
+        </div>
+        <div class="recipe-prop">
+            <p class="recipe-number">{{ (ingredient_prop[ingredient_id].health * ingredient_number).toFixed(2) }}<br></p>
+            <p class="recipe-prop-single">健康变化</p>
+        </div>
+        <div class="recipe-prop">
+            <p class="recipe-number">{{ (ingredient_prop[ingredient_id].stamina * ingredient_number).toFixed(2) }}<br></p>
+            <p class="recipe-prop-single">精力变化</p>
+        </div>
+        <div class="recipe-taste">
+            <p class="recipe-taste-text">味道 
+                <strong class="recipe-acid" v-if="ingredient_prop[ingredient_id].acid > 0">酸 </strong>
+                <strong class="recipe-sweet" v-if="ingredient_prop[ingredient_id].sweet > 0">甜 </strong>
+                <strong class="recipe-bitter" v-if="ingredient_prop[ingredient_id].bitter > 0">苦 </strong>
+                <strong class="recipe-salt" v-if="ingredient_prop[ingredient_id].salt > 0">咸 </strong>
+                <strong class="recipe-spice" v-if="ingredient_prop[ingredient_id].spice > 0">香料 </strong>
+                <strong class="recipe-none" v-if="ingredient_prop[ingredient_id].acid == 0 &&
+                    ingredient_prop[ingredient_id].sweet == 0 &&
+                    ingredient_prop[ingredient_id].bitter == 0 &&
+                    ingredient_prop[ingredient_id].salt == 0 &&
+                    ingredient_prop[ingredient_id].spice == 0">无 
+                </strong>
+            </p>
+        </div>
+        <button class="btn close" v-on:click="remove_ingredients">&times;</button>
+    </div>
+    `
+})
+
+Vue.component('ingredient-total', {
+    props: ["ingredient_total"],
+    data: function () {
+        return {
+            starvation: 0,
+            happiness: 0,
+            health: 0,
+            stamina: 0,
+            acid: 0,
+            sweet: 0,
+            bitter: 0,
+            salt: 0,
+            spice: 0
+        }
+    },
+    methods: {
+        get_total_props: function () {
+        }
+    },
+    watch: {
+        ingredient_total: function () {
+            console.log(this.ingredient_total);
+            this.get_total_props();
+        }
+    },
+    template: `
+    <div>
+        <p class="recipe-total-name">属性合计</p>
+        <div class="recipe-single bottomline-dashed">
             <div class="recipe-prop">
                 <p class="recipe-number">{{ starvation.toFixed(2) }}<br></p>
-                <p class="recipe-starvation">饥饿变化</p>
+                <p class="recipe-prop-single">饥饿变化</p>
             </div>
             <div class="recipe-prop">
                 <p class="recipe-number">{{ happiness.toFixed(2) }}<br></p>
-                <p class="recipe-health">快乐变化</p>
+                <p class="recipe-prop-single">快乐变化</p>
             </div>
             <div class="recipe-prop">
                 <p class="recipe-number">{{ health.toFixed(2) }}<br></p>
-                <p class="recipe-health">健康变化</p>
+                <p class="recipe-prop-single">健康变化</p>
             </div>
             <div class="recipe-prop">
                 <p class="recipe-number">{{ stamina.toFixed(2) }}<br></p>
-                <p class="recipe-stamina">精力变化</p>
+                <p class="recipe-prop-single">精力变化</p>
             </div>
             <div class="recipe-taste">
                 <p class="recipe-taste-text">味道 
-                    <strong class="recipe-acid" v-if="ingredient_prop[ingredient_id].acid > 0">酸 </strong>
-                    <strong class="recipe-sweet" v-if="ingredient_prop[ingredient_id].sweet > 0">甜 </strong>
-                    <strong class="recipe-bitter" v-if="ingredient_prop[ingredient_id].bitter > 0">苦 </strong>
-                    <strong class="recipe-salt" v-if="ingredient_prop[ingredient_id].salt > 0">咸 </strong>
-                    <strong class="recipe-spice" v-if="ingredient_prop[ingredient_id].spice > 0">香料 </strong>
-                    <strong class="recipe-none" v-if="ingredient_prop[ingredient_id].acid == 0 &&
-                        ingredient_prop[ingredient_id].sweet == 0 &&
-                        ingredient_prop[ingredient_id].bitter == 0 &&
-                        ingredient_prop[ingredient_id].salt == 0 &&
-                        ingredient_prop[ingredient_id].spice == 0">无 
-                    </strong>
+                    <strong class="recipe-acid" v-if="acid > 0">酸 </strong>
+                    <strong class="recipe-sweet" v-if="sweet > 0">甜 </strong>
+                    <strong class="recipe-bitter" v-if="bitter > 0">苦 </strong>
+                    <strong class="recipe-salt" v-if="salt > 0">咸 </strong>
+                    <strong class="recipe-spice" v-if="spice > 0">香料 </strong>
+                    <strong class="recipe-none" v-if="acid == 0 && sweet == 0 && bitter == 0 && salt == 0 && spice == 0">无 </strong>
                 </p>
             </div>
         </div>
-        `
-      })
-    recipe_vm = new Vue({
-        el: "#main",
-        data: {
+    </div>
+    `
+    })
+
+Vue.component('ingredient-all', {
+    data: function () {
+        return {
             next_number: 0,
             number_of_ingredients: [],
             used_ingredients: [],
@@ -178,31 +193,53 @@ function load_recipe()
                 17:{starvation:1,health:0.2,stamina:0,max:5,acid:5,sweet:15,bitter:0,salt:0,spice:0},
                 18:{starvation:2,health:0.2,stamina:0,max:5,acid:0,sweet:5,bitter:0,salt:0,spice:0},
             }
-        },
-        created: function () {
-            this.add_ingredients();
-        },
-        methods: {
-            add_ingredients: function (){
-                this.number_of_ingredients.push(this.next_number);
-                this.used_ingredients.push({id:-1,number:0});
-                this.ingredient_total.push({id:-1,number:0});
-                this.next_number++;
-            },
-            remove_ingredients: function (index){
-                this.number_of_ingredients.splice(index, 1);
-                this.used_ingredients.splice(index, 1);
-                this.ingredient_total.splice(index, 1);
-                this.next_number--;
-            },
-            ingredients_had_changed: function (index,ingredient_id,ingredient_number){
-                this.$set(this.used_ingredients,index,ingredient_id)
-                this.$set(this.ingredient_total,index,{id:ingredient_id,number:ingredient_number})
-            },
-            test: function (){
-                console.log(this.used_ingredients);
-                console.log(this.ingredient_list);
-            }
         }
-    })
-}
+    },
+    created: function () {
+        this.add_ingredients();
+    },
+    methods: {
+        add_ingredients: function (){
+            this.number_of_ingredients.push(this.next_number);
+            this.used_ingredients.push({id:-1,number:0});
+            this.ingredient_total.push({id:-1,number:0});
+            this.next_number++;
+        },
+        remove_ingredients: function (index){
+            this.number_of_ingredients.splice(index, 1);
+            this.used_ingredients.splice(index, 1);
+            this.ingredient_total.splice(index, 1);
+            this.next_number--;
+        },
+        ingredients_had_changed: function (index,ingredient_id,ingredient_number){
+            this.$set(this.used_ingredients,index,ingredient_id)
+            this.$set(this.ingredient_total,index,{id:ingredient_id,number:ingredient_number})
+        },
+        test: function (){
+            console.log(this.used_ingredients);
+            console.log(this.ingredient_list);
+        }
+    },
+    template: `
+    <div class="main-double">
+        <p class="main-char">新建食谱</p>
+        <p class="author">创建全新的食谱以供享用。</p>
+        <input type="text" class="form-control" placeholder="食谱名"></input>
+        <button class="btn btn-success">保存食谱</button>
+        <button class="btn btn-warning">取消</button>
+        <ingredient-detail 
+            v-for="(ingredient_id,index) in number_of_ingredients" 
+            v-on:test="test" 
+            v-on:remove="remove_ingredients" 
+            v-on:ingredient_change="ingredients_had_changed"
+            v-bind:ingredient_list="ingredient_list" 
+            v-bind:used_ingredients="used_ingredients" 
+            v-bind:ingredient_prop="ingredient_prop" 
+            v-bind:index="index" 
+            v-bind:key="ingredient_id">
+        </ingredient-detail>
+        <button class="btn btn-success" v-on:click="add_ingredients">添加食材</button>
+        <ingredient-total v-bind:ingredient_total="ingredient_total"></ingredient-total>
+    </div>
+    `
+})
