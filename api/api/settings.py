17a2e22bf230db7da503020fc65a30c1339ac1e1
Shiroko <hhx.xxm@gmail.com>
2021-09-18 01:09:00 +0800

    change file mode

--- api/api/settings.py
+++ api/api/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'WEHAVENOSECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'civitas',
         'HOST': '',
-        'PORT': '',
+        'PORT': '3306',
         'USER': 'root',
         'PASSWORD': '',
         'OPTIONS': { 
