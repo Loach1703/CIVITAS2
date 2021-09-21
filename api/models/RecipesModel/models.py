7396b5806b31903760e935f2b5f9efa60fbd24d8
fengwewn <2324465096@qq.com>
2021-09-21 19:17:21 +0800

    食谱表2

--- api/models/RecipesModel/models.py
+++ api/models/RecipesModel/models.py
@@ -39,8 +39,88 @@ from django.db.models.fields.related_descriptors import ManyToManyDescriptor
 #     # class Meta:
 #     #     unique_together = [
 #     #         'material','level'
+
 #     #     ]
+
+# class diet_MaterialDetail(models.Model):
+#     level_choices = ((1, 'Q1'), (2, 'Q2'),(3, 'Q3'))
+#     #material = ForeignKey('Material',on_delete=models.CASCADE,verbose_name='物资')
+#     productivity = FloatField(verbose_name='物资自身产能')
+#     level = SmallIntegerField(verbose_name='物资等级',default=1,choices=level_choices)
+
+#     # class Meta:
+#     #     unique_together = [
+#     #         'level'
+#     #     ]
+
+#     def __str__(self):
+#         return self.material.name+' Q.'+str(self.level)
+
+# class Input_Recipe_Material(models.Model):
+#     recipe = ForeignKey('diet_recipe',on_delete=models.CASCADE,verbose_name='配方')
+#     material = ForeignKey('diet_MaterialDetail',on_delete=models.CASCADE,verbose_name='输入物资')
+#     count = IntegerField(verbose_name='数量')
+#     class Meta:
+#         verbose_name_plural = '所需食材表'
+
+# class diet_recipe(models.Model):
+#     name = CharField(max_length=50,verbose_name='名字',default=" ")
+#     Owner = IntegerField(db_index=True,verbose_name='拥有者',default=" ") #拥有者
+#     input = ManyToManyField('diet_MaterialDetail',related_name='input',verbose_name='输入',through=Input_Recipe_Material)
+#     health = FloatField(verbose_name='健康度',default=0.00)
+#     Satiety = FloatField(verbose_name='饱食度',default=0.00)
+#     acid = FloatField(verbose_name='酸',default=0.00)
+#     salty = FloatField(verbose_name='咸',default=0.00)
+#     sweet = FloatField(verbose_name='甜',default=0.00)
+#     bitterness= FloatField(verbose_name='苦',default=0.00)
+#     aroma = FloatField(verbose_name='味道度',default=0.00)
+    
+
+# class diet_materialDetail(models.Model):
+#     #食材表
+#     #id = IntegerField(primary_key=True)
+#     level_choices = ((1, 'Q1'), (2, 'Q2'),(3, 'Q3'))
+#     level = SmallIntegerField(verbose_name='物资等级',default=1,choices=level_choices)
+#     class Meta:
+#         unique_together = [
+#             'level'
+#         ]
+
+# class Input_Recipe_Material_1(models.Model):
+#     recipe = ForeignKey('diet_recipes',on_delete=models.CASCADE,verbose_name='配方id')
+#     material = ForeignKey('diet_materialDetail',on_delete=models.CASCADE,verbose_name='输入物资')
+#     count = IntegerField(verbose_name='数量')
  
+# class diet_recipes(models.Model):
+
+#     input = ManyToManyField('Input_Recipe_Material',related_name='input',verbose_name='输入',through=Input_Recipe_Material)
+    #input = ManyToManyField('diet_materialDetail',related_name='input',through=Input_Recipe_Material)
+
+
+# from django.db import models
+# # Create your models here.
+ 
+# # 手动创建第三张表,查询还方便的查询
+# class Book(models.Model):
+#     # 默认会创建id
+#     name = models.CharField(max_length=32)
+#     # 中介模型,手动指定第三张中间表是Book2Author
+#     authors = models.ManyToManyField(to='Author', through='Book2Author', through_fields=('book', 'author'))
+ 
+ 
+# class Author(models.Model):
+#     name = models.CharField(max_length=32)
+ 
+#     def __str__(self):
+#         return self.name
+ 
+ 
+# class Book2Author(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     book = models.ForeignKey(to='Book', to_field='id',on_delete=models.CASCADE,)
+#     author = models.ForeignKey(to='Author', to_field='id',on_delete=models.CASCADE)
+
+
 
 
 
