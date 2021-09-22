a118ac1ddeb367b7ab39906e3436f0a721cb2f53
fengwewn <2324465096@qq.com>
2021-09-22 20:39:04 +0800

    1

---
+++ api/api/function/diet.py
@@ -0,0 +1,27 @@
+from django.db.models.expressions import F
+from django.db.models.query_utils import Q
+from django.shortcuts import redirect,render,HttpResponse
+from assist import *
+from django.contrib.sessions.models import Session
+from django.contrib import auth
+from MaterialModel.models import *
+from UserModel.models import *
+from WorkModel.models import *
+import random
+import json
+import datetime
+from DietModel.models import *
+
+
+def GetUserRecipes(req):
+    sessionid = req.COOKIES.get("sessionid")
+    uid = req.GET.get("uid")
+    user = auth.models.User.objects.get(pk=uid)
+    
+
+
+
+
+
+        
+    
