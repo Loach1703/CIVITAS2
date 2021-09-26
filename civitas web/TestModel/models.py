caf3ac89c3755bfa296435d0df48f9ee4c24b3c3
TrickyDeath <1274082815@qq.com>
2021-09-26 20:06:20 +0800

    前端弃用django框架
    前端弃用django框架

--- civitas web/TestModel/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# class speech(models.Model):
-#     #name = models.CharField(max_length=20)
-#     text = models.CharField(max_length=280)
-#     date = models.DateTimeField()
-
-#计划，用户表单的扩展，包括电话，随机长id等等。
